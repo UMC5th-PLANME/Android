59e09bb4bd4da2b2cd85be8405fc6392f64fb57a
yummpotato <hevlyoy26@naver.com>
2024-01-02 16:02:19 +0900

    Add: Login View 추가

--- app/src/main/java/com/example/plan_me/LoginActivity.kt
+++ app/src/main/java/com/example/plan_me/LoginActivity.kt
@@ -1,4 +1,15 @@
 package com.example.plan_me
 
-class LoginActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.plan_me.databinding.ActivityLoginBinding
+
+class LoginActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
