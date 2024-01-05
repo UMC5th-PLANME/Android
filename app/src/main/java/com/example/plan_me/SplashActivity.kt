ad0d313a95cb4eeb0a74e671e341c8aff5539084
yummpotato <hevlyoy26@naver.com>
2024-01-06 00:18:08 +0900

    Add: Planner View 구현

--- app/src/main/java/com/example/plan_me/SplashActivity.kt
+++ app/src/main/java/com/example/plan_me/SplashActivity.kt
@@ -3,13 +3,12 @@ package com.example.plan_me
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.plan_me.databinding.ActivityMainBinding
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val intent = Intent(this, MainActivity::class.java)
+        val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
         finish()
     }
