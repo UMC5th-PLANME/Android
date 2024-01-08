a3a056ecd1a063dd63f4f36d84ea959746b80c45
fizzyseo <whatafineday23@gmail.com>
2024-01-09 01:29:54 +0900

    Fix: .gitignore 캐시 삭제

--- app/build/tmp/kapt3/stubs/debug/com/example/plan_me/ui/mestory/MestoryRVAdapter.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/plan_me/ui/mestory/MestoryRVAdapter.java
@@ -3,7 +3,7 @@ package com.example.plan_me.ui.mestory;
 @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u00002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0011\n\u0002\u0010\u000e\n\u0002\b\u0004\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0002\b\u0003\u0018\u00002\f\u0012\b\u0012\u00060\u0002R\u00020\u00000\u0001:\u0001\u0014B\u0005\u00a2\u0006\u0002\u0010\u0003J\b\u0010\n\u001a\u00020\u000bH\u0016J\u001c\u0010\f\u001a\u00020\r2\n\u0010\u000e\u001a\u00060\u0002R\u00020\u00002\u0006\u0010\u000f\u001a\u00020\u000bH\u0016J\u001c\u0010\u0010\u001a\u00060\u0002R\u00020\u00002\u0006\u0010\u0011\u001a\u00020\u00122\u0006\u0010\u0013\u001a\u00020\u000bH\u0016R\u0019\u0010\u0004\u001a\b\u0012\u0004\u0012\u00020\u00060\u0005\u00a2\u0006\n\n\u0002\u0010\t\u001a\u0004\b\u0007\u0010\b\u00a8\u0006\u0015"}, d2 = {"Lcom/example/plan_me/ui/mestory/MestoryRVAdapter;", "Landroidx/recyclerview/widget/RecyclerView$Adapter;", "Lcom/example/plan_me/ui/mestory/MestoryRVAdapter$ViewHolder;", "()V", "likeData", "", "", "getLikeData", "()[Ljava/lang/String;", "[Ljava/lang/String;", "getItemCount", "", "onBindViewHolder", "", "holder", "position", "onCreateViewHolder", "viewGroup", "Landroid/view/ViewGroup;", "viewType", "ViewHolder", "app_debug"})
 public final class MestoryRVAdapter extends androidx.recyclerview.widget.RecyclerView.Adapter<com.example.plan_me.ui.mestory.MestoryRVAdapter.ViewHolder> {
     @org.jetbrains.annotations.NotNull
-    private final java.lang.String[] likeData = {"1.4\ub9cc", "2.3\ub9cc"};
+    private final java.lang.String[] likeData = {"Exercise", "Study"};
     
     public MestoryRVAdapter() {
         super();
