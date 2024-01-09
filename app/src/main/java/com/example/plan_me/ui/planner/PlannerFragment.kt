91b3462d4325bbffad52fbab237bcfd534036b7f
fizzyseo <whatafineday23@gmail.com>
2024-01-09 22:59:17 +0900

    Test: PlannerFragment에 test22 추가

--- app/src/main/java/com/example/plan_me/ui/planner/PlannerFragment.kt
+++ app/src/main/java/com/example/plan_me/ui/planner/PlannerFragment.kt
@@ -11,6 +11,7 @@ import com.example.plan_me.databinding.FragmentPlannerBinding
 
 class PlannerFragment : Fragment() {
     private lateinit var binding: FragmentPlannerBinding
+    private var test22 = 1
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
