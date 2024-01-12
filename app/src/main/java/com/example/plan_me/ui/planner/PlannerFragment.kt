cada22837c2a416f45fdd7d20230cad3cfaa5767
fizzyseo <whatafineday23@gmail.com>
2024-01-12 09:40:37 +0900

    Add: Floating Action Button으로 화면 이동

--- app/src/main/java/com/example/plan_me/ui/planner/PlannerFragment.kt
+++ app/src/main/java/com/example/plan_me/ui/planner/PlannerFragment.kt
@@ -11,7 +11,7 @@ import com.example.plan_me.databinding.FragmentPlannerBinding
 
 class PlannerFragment : Fragment() {
     private lateinit var binding: FragmentPlannerBinding
-    private var test22 = 4
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
