a09595e649d744acffd0be5bfcacb89e78b6b31b
RakunKo <huncher00@naver.com>
2024-01-14 21:54:20 +0900

    Fix: Fab size, frag->acti

--- app/src/main/java/com/example/plan_me/ui/dialog/DialogCalenderInterface.kt
+++
@@ -1,7 +0,0 @@
-package com.example.plan_me.ui.dialog
-
-import java.time.LocalDate
-
-interface DialogCalenderInterface {
-    fun onClickCalenderConfirm(start : LocalDate?, end:LocalDate?)
-}
--- app/src/main/java/com/example/plan_me/ui/dialog/DialogTimeRangePickInerface.kt
+++
@@ -1,6 +0,0 @@
-package com.example.plan_me.ui.dialog
-
-interface DialogTimeRangePickInerface {
-    fun onRangeClickConfirm(startTime : String, endTime:String)
-    fun onRangeClickCancel()
-}
