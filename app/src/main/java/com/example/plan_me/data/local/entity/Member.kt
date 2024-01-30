5f4a1bea6d8bbfe14b5220b2d5efc1f25a392b01
yummpotato <hevlyoy26@naver.com>
2024-01-30 17:25:20 +0900

    Fix: 로그아웃 구현, accessToken + email 정보 받아오기

--- app/src/main/java/com/example/plan_me/data/local/entity/Member.kt
+++ app/src/main/java/com/example/plan_me/data/local/entity/Member.kt
@@ -1,6 +1,12 @@
 package com.example.plan_me.data.local.entity
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Member")
 data class Member(
+    @PrimaryKey val email: String,
     val name: String,
-    val image_url: String
+    val profile_image: String,
+    val login_type: String
 )
--- app/src/main/java/com/example/plan_me/data/local/entity/Terms.kt
+++ app/src/main/java/com/example/plan_me/data/local/entity/Terms.kt
@@ -1,7 +1,11 @@
 package com.example.plan_me.data.local.entity
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Terms")
 data class Terms(
-    val member_id: Int,
+    @PrimaryKey val member_id: Int,
     val agreeTermIds: Array<Int>,
     val disagreeTermIds: Array<Int>
 )
