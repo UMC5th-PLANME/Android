0b52762bb2da1f116f0211e68d055833cc296fc9
RakunKo <huncher00@naver.com>
2024-02-04 22:41:30 +0900

    Fix: 뒤로가기 안되게

--- app/src/main/java/com/example/plan_me/ui/login/InitProfileActivity.kt
+++ app/src/main/java/com/example/plan_me/ui/login/InitProfileActivity.kt
@@ -170,6 +170,7 @@ class InitProfileActivity : AppCompatActivity(), ProfileImageView, ChangeProfile
 
     private fun goMainActivity() {
         val intent = Intent(this@InitProfileActivity, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
         startActivity(intent)
         overridePendingTransition(R.anim.screen_none, R.anim.screen_exit)
     }
