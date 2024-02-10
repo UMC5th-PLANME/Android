c4607c359d15afacb317eb1f5c61fe48df085b82
RakunKo <huncher00@naver.com>
2024-02-10 22:23:52 +0900

    Add: 일정 삭제, 수정, 수정 필요

--- app/src/main/java/com/example/plan_me/data/remote/view/timer/TimerView.kt
+++
@@ -1,8 +0,0 @@
-package com.example.plan_me.data.remote.view.timer
-
-import com.example.plan_me.data.remote.dto.timer.TimerSettingRes
-
-interface TimerView {
-    fun onSetTimerSuccess(response: TimerSettingRes)
-    fun onSetTimerFailure(isSuccess: Boolean, code: String, message: String)
-}
--- app/src/main/java/com/example/plan_me/ui/dialog/DialogYMPickInerface.kt
+++
@@ -1,5 +0,0 @@
-package com.example.plan_me.ui.dialog
-
-interface DialogYMPickInerface {
-    fun onClickConfirm(year : String?, month:String?)
-}
