f7b06b63721a2462434c47e9500ce8a1ca9d53f2
fizzyseo <whatafineday23@gmail.com>
2024-02-18 23:04:44 +0900

    Add: Mestory 설정 카테고리 불러오기

--- app/src/main/java/com/example/plan_me/ui/setting/MestorySettingFragment.kt
+++ app/src/main/java/com/example/plan_me/ui/setting/MestorySettingFragment.kt
@@ -1,6 +1,7 @@
 package com.example.plan_me.ui.setting
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,11 +12,13 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.plan_me.R
 import com.example.plan_me.data.remote.dto.category.AllCategoryRes
 import com.example.plan_me.data.remote.dto.category.CategoryList
+import com.example.plan_me.data.remote.dto.category.ModifyStatusCategoryRes
 import com.example.plan_me.data.remote.service.category.CategoryService
 import com.example.plan_me.data.remote.view.category.AllCategoryView
+import com.example.plan_me.data.remote.view.category.ModifyStatusCategoryView
 import com.example.plan_me.databinding.ActivityManageMestoryBinding
 
-class MestorySettingFragment: Fragment(), AllCategoryView {
+class MestorySettingFragment: Fragment(), AllCategoryView, ModifyStatusCategoryView {
     private lateinit var binding: ActivityManageMestoryBinding
     private lateinit var categoryList : List<CategoryList>
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -61,11 +64,21 @@ class MestorySettingFragment: Fragment(), AllCategoryView {
     }
 
     override fun onAllCategorySuccess(response: AllCategoryRes) {
+        Log.d("MestorySettingFragment", "Category 불러오기 설공")
         categoryList = response.result.categoryList
         initRV()
     }
 
     override fun onAllCategoryFailure(response: AllCategoryRes) {
-        TODO("Not yet implemented")
+        Log.d("MestorySettingFragment", "Category 불러오기 실패")
+    }
+
+    override fun onModifyStatusCategorySuccess(response: ModifyStatusCategoryRes) {
+        Log.d("MestorySettingFragment", "Category에서 숨김 상태 변경 성공")
+
+    }
+
+    override fun onModifyStatusCategoryFailure(response: ModifyStatusCategoryRes) {
+        Log.d("MestorySettingFragment", "Category에서 숨김 상태 변경 실패")
     }
 }
