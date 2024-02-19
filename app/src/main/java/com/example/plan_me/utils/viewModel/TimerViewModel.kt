38f9d3b7861bab23e5e2b48f520e155152dd3785
fizzyseo <whatafineday23@gmail.com>
2024-02-19 16:43:24 +0900

    Fix: 이전 커밋으로 돌아감

--- app/src/main/java/com/example/plan_me/utils/viewModel/TimerViewModel.kt
+++ app/src/main/java/com/example/plan_me/utils/viewModel/TimerViewModel.kt
@@ -12,17 +12,17 @@ import com.example.plan_me.data.remote.view.timer.GetTimerView
 class TimerViewModel(private val getResSharedPreferences: SharedPreferences, private val categoryIdSharedPreferences: SharedPreferences): ViewModel(), GetTimerView {
     val focusML = MutableLiveData<String>().apply {
         Log.d("init focus mutable livedata", "focus 라이브 데이터 초기화")
-        setValue("10:00:00")
+        setValue("00:00:00")
     }
 
     val breakML = MutableLiveData<String>().apply {
         Log.d("init break mutable livedata", "break 라이브 데이터 초기화")
-        setValue("10:00:00")
+        setValue("00:00:00")
     }
 
     val repeatCntML = MutableLiveData<Int>().apply {
         Log.d("init repeatCnt mutable livedata", "repeatCnt 라이브 데이터 초기화")
-        setValue(0)
+        setValue(1)
     }
 
     fun getFocus(): LiveData<String> = focusML
@@ -41,12 +41,16 @@ class TimerViewModel(private val getResSharedPreferences: SharedPreferences, pri
     override fun onGetTimerSuccess(response: GetTimerRes) {
         Log.d("TIMER VIEW 설정", response.result.toString())
 
-        focusML.value = response.result.focusTime
-        breakML.value = response.result.breakTime
-        repeatCntML.value = response.result.repeatCnt
+        focusML.postValue(response.result.focusTime)
+        breakML.postValue(response.result.breakTime)
+        repeatCntML.postValue(response.result.repeatCnt)
     }
 
     override fun onGetTimerFailure(isSuccess: Boolean, code: String, message: String) {
         Log.d("TIMER VIEW 설정 실패", message)
+
+        focusML.postValue("00:00:00")
+        breakML.postValue("00:00:00")
+        repeatCntML.postValue(1)
     }
 }
